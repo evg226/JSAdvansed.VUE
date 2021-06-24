4a04817570fae89e60e2cc68f9dfed8c498d82f7
evg226 <evg226@hotmail.com>
2021-06-24 10:25:32 +0500

    Добавлен лог действий с корзиной

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express=require("express");
 const app=express();
 const bodyParser=require("body-parser")
+const moment=require("moment");
 
 app.listen(8080,()=>console.log("Интернет магазин VUE запущен"));
 app.use(express.static("."));
 app.use(bodyParser.json());
 
+class Stats{
+    constructor(path="./data/stats.json"){
+        this.path=path;
+        this.stats=[];
+        this.openFile();
+    }
+    openFile(){
+        fs.readFile(this.path,"utf-8",(err,result)=>{
+            if(!err){
+                let data=JSON.parse(result);
+                this.stats=[...data];
+
+            } else{
+                console.log(`Ошибка при открытии лог-файла: ${err}`);
+            }
+        });
+    }
+    writeStat(action,cartItem){
+        const now=moment().format("YYYY-MM-DD hh:mm:ss");
+        this.stats.push({action,id:cartItem.id_product,name:cartItem.product_name,time:now});
+        fs.writeFile(this.path,JSON.stringify(this.stats),err=>{
+            err&&console.log(err);
+        });
+        console.log(action+": "+cartItem.product_name);
+    }
+}
+
 const fs=require("fs");
+const stats=new Stats();
 
 app.get("/catalog",(req,res)=>{
     fs.readFile("./data/catalog.json","utf-8",(err,result)=>{
@@ -26,18 +55,20 @@ app.post("/addToCart",(req,res)=>{
             const product=req.body;
             const cart=JSON.parse(result);
             let findItem=cart.find(cartItem=>product.id_product == cartItem.id_product);
+            let currentAction="";
             if(findItem){
                 findItem.quantity=product.hasOwnProperty("quantity")?product.quantity:+findItem.quantity+1;
-                console.log(`Изменение позиции: ${product.product_name}`);
+                currentAction="Изменение";
             } else {
                 cart.push({...product, quantity:1});
-                console.log(`Добавление позиции: ${product.product_name}`);
+                currentAction="Добавление";
             }
             fs.writeFile("./data/cart.json",JSON.stringify(cart),err=>{
                 if (err){
                     res.send('{"result": 0}');
                 } else {
                     res.send('{"result": 1}');
+                    stats.writeStat(currentAction,product);
                 }
             });
         }
@@ -49,7 +80,6 @@ app.post("/removeFromToCart",(req,res)=>{
             res.send('{"result": 0}');
         } else {
             const cartItem=req.body;
-            console.log(`Удаление: ${cartItem.product_name}`);
             const cart=JSON.parse(result);
             cart.splice(cart.indexOf(cartItem),1);
             fs.writeFile("./data/cart.json",JSON.stringify(cart),err=>{
@@ -57,9 +87,15 @@ app.post("/removeFromToCart",(req,res)=>{
                     res.send('{"result": 0}');
                 } else {
                     res.send('{"result": 1}');
+                    stats.writeStat("Удаление",cartItem);
                 }
             });
         }
     })
 });
+
+
+
+
+
 
